56e26ccb383d904d52ec4ffaf983772797ad572f
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-11-22 14:14:18 -0600

    changed static_root var

--- FitLyfeDjango/FitLyfeDjango/settings.py
+++ FitLyfeDjango/FitLyfeDjango/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT= '../docs'
+STATIC_ROOT= os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = '/Users/Bwilburn/workSpace/FitLyfe/FitLyfeDjango/instructional_videos'
